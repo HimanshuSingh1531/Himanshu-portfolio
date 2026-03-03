42cbd9f400cfd29364dca59825362223a9504bb5
HimanshuSingh <himanshus85549@gmail.com>
2026-03-04 00:46:03 +0530

    Replaced emailjs-com with @emailjs/browser

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Mail, Github, Linkedin, Twitter, Instagram, Facebook, Link, BookOpen, MessageCircle, Users } from 'lucide-react';
 import ParticleBackground from '../ParticleBackground';
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 
 const ThreadsIcon = ({ className }) => (
   <svg
